b75a45b35aad86ac6caa271c1685e69ab643464a
arxarian <arxarian@gmail.com>
2018-11-10 20:17:34 +0100

    replace tabs by spaces
    Signed-off-by: arxarian <arxarian@gmail.com>

--- apps.js
+++ apps.js
@@ -4,34 +4,40 @@ var app = express();
 const pg = require('pg');
 
 var config = {
-  user: 'measuser', // env var: PGUSER
-  database: 'measurements', // env var: PGDATABASE
-  password: 'measuser', // env var: PGPASSWORD
-  host: 'localhost', // Server hosting the postgres database
-  port: 5432, // env var: PGPORT
-  max: 10, // max number of clients in the pool
-  idleTimeoutMillis: 30000 // how long a client is allowed to remain idle before being closed
+    user: 'measuser', // env var: PGUSER
+    database: 'measurements', // env var: PGDATABASE
+    password: 'measuser', // env var: PGPASSWORD
+    host: 'localhost', // Server hosting the postgres database
+    port: 5432, // env var: PGPORT
+    max: 10, // max number of clients in the pool
+    idleTimeoutMillis: 30000 // how long a client is allowed to remain idle before being closed
 }
 
 app.set('port', 80);
 
 app.get('/', function (req, res) {
-	var pool = new pg.Pool(config);
+    console.time("request")
+    var pool = new pg.Pool(config);
 
-	pool.connect(function(err, client, done) {
-		if (err) {
-			console.log("error:", err)
-		}
+    pool.connect(function(err, client, done) {
+        if (err) {
+            console.log("error:", err)
+        }
+        console.time("connect")
 
-		client.query('SELECT * FROM basecamp ORDER BY timestamp DESC LIMIT 3', function (err, result) {
-			done()
-			res.send(result.rows)
-		})
-	})
+        client.query('SELECT * FROM basecamp ORDER BY timestamp DESC LIMIT 3', function (err, result) {
+            done()
+            console.time("end")
+            res.send(result.rows)
+            console.timeEnd("end")
+            console.timeEnd("connect")
+            console.timeEnd("request")
+        })
+    })
 
-	pool.end()
+    pool.end()
 });
 
 app.listen(app.get('port'), function () {
-	console.log('server started on port ' + app.get('port'));
+    console.log('server started on port ' + app.get('port'));
 });
